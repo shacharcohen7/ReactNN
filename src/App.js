eb447235a7845a6c16d9388e457eede82bf89170
shachar <shachco@post.bgu.ac.il>
2024-11-29 13:41:13 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-logo">
+        <div className="image-layer"></div> {/* שכבת התמונה */}
+        <div className="content">
+          <h1 className="welcome-title">ברוכים הבאים!</h1>
+
+          {/* טופס התחברות */}
+          <div className="login-container">
+            <p className="instruction">נא להזין שם משתמש וסיסמה אוניברסיטאיים</p>
+            <form className="login-form">
+              <div className="input-group">
+                <label htmlFor="username">שם משתמש</label>
+                <input type="text" id="username" placeholder="הזן שם משתמש" required />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">סיסמה</label>
+                <input type="password" id="password" placeholder="הזן סיסמה" required />
+              </div>
+              <button type="submit" className="login-button">התחברות</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
