91f131616e60c81fecb3ff97edac40ccde4ce2ad
DavidVolodarsky1 <david.volodarski1@gmail.com>
2024-12-19 02:14:07 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';  // ייבוא App
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  // ייבוא App
+import App from './App'; // Import App
+import { UserProvider } from './context/UserContext'; // Import UserProvider
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider> {/* Wrap App with UserProvider */}
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
 
