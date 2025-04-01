d61321361e108c616bfa0f84794c86684b7f0d0a
nadavktalav <103999790+nadavktalav@users.noreply.github.com>
2025-04-01 15:59:07 +0300

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // יצירת axios instance
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api/',  // כתובת ה-API של השרת
+    baseURL: 'https://negevnerds.cs.bgu.ac.il:5001/api/',  // כתובת ה-API של השרת
     headers: {
         'Content-Type': 'application/json',
     }
